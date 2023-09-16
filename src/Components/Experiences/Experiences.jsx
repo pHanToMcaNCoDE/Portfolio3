8f9969606c684064456c4c3b880a24593e997b7e
Victory Odumeh <vodumeh@gmail.com>
2023-09-16 18:02:31 +0100

    experience page is complete

--- src/Components/Experiences/Experiences.jsx
+++ src/Components/Experiences/Experiences.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
+import ExpDisplay from '../ExpDisplay/ExpDisplay'
+import Footer from '../Footer/Footer'
 import Navbar from '../Navbar/Navbar'
 import './exp.css'
 
 const Experiences = () => {
   return (
-    <div className='bg-neutral-950 w-full h-screen mx-auto p-2 text-white'>
+    <div className='bg-neutral-950 w-full lg:h-[140vh] text-white'>
       <Navbar/>
-      <h1 className='font-mono text-center text-[3rem] text-purple-500 tracking-widest header'>Experiences</h1>
+      <div className='max-w-[1200px] mx-auto p-2'>
+          <h1 className='font-mono text-center text-[2rem] lg:text-[3rem] text-purple-500 tracking-widest header'>Experiences</h1>
 
-      <div>
-        
+          <div className='text-center'>
+            <p className='p w-[90%] lg:w-[60%] p-1 mx-auto font-mono text-[1.1rem]tracking-wide leading-8'>Detailed-oriented Developer capable of crafting aesthetically pleasing and responsive UI applications.</p>
+          </div>
+
+          <ExpDisplay/>
+          <Footer/>
+
       </div>
 
     </div>
