64965fe248bca5d8962a367c430f573e57eca7e4
Victory Odumeh <vodumeh@gmail.com>
2023-09-16 18:19:51 +0100

    working on project display cards

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Footer from '../Footer/Footer'
 import Navbar from '../Navbar/Navbar'
+import { projects } from '../../Data/Data'
 
 const Portfolio = () => {
   return (
@@ -11,107 +12,27 @@ const Portfolio = () => {
 
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-[5%] gap-8'>
 
-            <div className='bg-zinc-900 w-[100%] after:rounded-lg after:duration-300 after:w-1 after:inline-flex after:h-[1px] after:bg-white hover:after:w-full rounded-xl'>
-                <div className='flex justify-between items-center p-4'>
-                    <h1 className='text-left font-mono text-[1.3rem]'>Project Name</h1>
-                    <a className='font-mono text-[.8rem] tracking-wider' href='#' target='_blank'>View Project</a>
-                </div>
-                
-                <p className='p-4 font-mono text-[.9rem] text-justify text-gray-300'>Cupidatat in tempor nulla irure eiusmod aliqua incididunt velit. Deserunt Lorem reprehenderit ullamco ea id sit ex enim nulla mollit tempor dolore.</p>
-                <div className='flex justify-between items-center px-4'>
-                    <div className='flex items-center'>
-                        <div className='w-2 h-2 rounded-full bg-blue-500'></div>
-                        <p className='ml-3 font-mono text-[.9rem] tracking-widest'>ReactJS</p>
+            {
+                projects.map(item => (
+                    <div className='bg-zinc-900 w-[100%] after:rounded-lg after:duration-300 after:w-1 after:inline-flex after:h-[1px] after:bg-white hover:after:w-full rounded-xl'>
+                        <div className='flex justify-between items-center p-4'>
+                            <h1 className='text-left font-mono text-[1.3rem]'>{item.name}</h1>
+                            <a className='font-mono text-[.8rem] tracking-wider' href='#' target='_blank'>View Project</a>
+                        </div>
+                        
+                        <p className='p-4 font-mono text-[.9rem] text-justify text-gray-300'>Cupidatat in tempor nulla irure eiusmod aliqua incididunt velit. Deserunt Lorem reprehenderit ullamco ea id sit ex enim nulla mollit tempor dolore.</p>
+                        <div className='flex justify-between items-center px-4'>
+                            <div className='flex items-center'>
+                                <div style={{ backgroundColor: item.color }} className='w-2 h-2 rounded-full'></div>
+                                <p className='ml-3 font-mono text-[.9rem] tracking-widest'>{item.tool}</p>
+                            </div>
+                            <a className='font-mono text-[.8rem] tracking-wider' href='#' target='_blank'>GitHub</a>
+                        </div>
                     </div>
-                    <a className='font-mono text-[.8rem] tracking-wider' href='#' target='_blank'>GitHub</a>
-                </div>
-            </div>
+                ))
+            }
 
-
-            <div className='bg-zinc-900 w-[100%] after:rounded-lg after:duration-300 after:w-1 after:inline-flex after:h-[1px] after:bg-white hover:after:w-full rounded-xl'>
-                <div className='flex justify-between items-center p-4'>
-                    <h1 className='text-left font-mono text-[1.3rem]'>Project Name</h1>
-                    <a className='font-mono text-[.8rem] tracking-wider' href='#' target='_blank'>View Project</a>
-                </div>
-                
-                <p className='p-4 font-mono text-justify text-[.9rem] text-gray-300'>Cupidatat in tempor nulla irure eiusmod aliqua incididunt velit. Deserunt Lorem reprehenderit ullamco ea id sit ex enim nulla mollit tempor dolore.</p>
-                <div className='flex justify-between items-center px-4'>
-                    <div className='flex items-center'>
-                        <div className='w-2 h-2 rounded-full bg-blue-500'></div>
-                        <p className='ml-3 font-mono text-[.9rem] tracking-widest'>ReactJS</p>
-                    </div>
-                    <a className='font-mono text-[.8rem] tracking-wider' href='#' target='_blank'>GitHub</a>
-                </div>
-            </div>
-
-
-            <div className='bg-zinc-900 w-[100%] after:rounded-lg after:duration-300 after:w-1 after:inline-flex after:h-[1px] after:bg-white hover:after:w-full rounded-xl'>
-                <div className='flex justify-between items-center p-4'>
-                    <h1 className='text-left font-mono text-[1.3rem]'>Project Name</h1>
-                    <a className='font-mono text-[.8rem] tracking-wider' href='#' target='_blank'>View Project</a>
-                </div>
-                
-                <p className='p-4 font-mono text-justify text-[.9rem] text-gray-300'>Cupidatat in tempor nulla irure eiusmod aliqua incididunt velit. Deserunt Lorem reprehenderit ullamco ea id sit ex enim nulla mollit tempor dolore.</p>
-                <div className='flex justify-between items-center px-4'>
-                    <div className='flex items-center'>
-                        <div className='w-2 h-2 rounded-full bg-blue-500'></div>
-                        <p className='ml-3 font-mono text-[.9rem] tracking-widest'>ReactJS</p>
-                    </div>
-                    <a className='font-mono text-[.8rem] tracking-wider' href='#' target='_blank'>GitHub</a>
-                </div>
-            </div>
-
-
-            <div className='bg-zinc-900 w-[100%] after:rounded-lg after:duration-300 after:w-1 after:inline-flex after:h-[1px] after:bg-white hover:after:w-full rounded-xl'>
-                <div className='flex justify-between items-center p-4'>
-                    <h1 className='text-left font-mono text-[1.3rem]'>Project Name</h1>
-                    <a className='font-mono text-[.8rem] tracking-wider' href='#' target='_blank'>View Project</a>
-                </div>
-                
-                <p className='p-4 font-mono text-justify text-[.9rem] text-gray-300'>Cupidatat in tempor nulla irure eiusmod aliqua incididunt velit. Deserunt Lorem reprehenderit ullamco ea id sit ex enim nulla mollit tempor dolore.</p>
-                <div className='flex justify-between items-center px-4'>
-                    <div className='flex items-center'>
-                        <div className='w-2 h-2 rounded-full bg-blue-500'></div>
-                        <p className='ml-3 font-mono text-[.9rem] tracking-widest'>ReactJS</p>
-                    </div>
-                    <a className='font-mono text-[.8rem] tracking-wider' href='#' target='_blank'>GitHub</a>
-                </div>
-            </div>
-
-
-            <div className='bg-zinc-900 w-[100%] after:rounded-lg after:duration-300 after:w-1 after:inline-flex after:h-[1px] after:bg-white hover:after:w-full rounded-xl'>
-                <div className='flex justify-between items-center p-4'>
-                    <h1 className='text-left font-mono text-[1.3rem]'>Project Name</h1>
-                    <a className='font-mono text-[.8rem] tracking-wider' href='#' target='_blank'>View Project</a>
-                </div>
-                
-                <p className='p-4 font-mono text-justify text-[.9rem] text-gray-300'>Cupidatat in tempor nulla irure eiusmod aliqua incididunt velit. Deserunt Lorem reprehenderit ullamco ea id sit ex enim nulla mollit tempor dolore.</p>
-                <div className='flex justify-between items-center px-4'>
-                    <div className='flex items-center'>
-                        <div className='w-2 h-2 rounded-full bg-blue-500'></div>
-                        <p className='ml-3 font-mono text-[.9rem] tracking-widest'>ReactJS</p>
-                    </div>
-                    <a className='font-mono text-[.8rem] tracking-wider' href='#' target='_blank'>GitHub</a>
-                </div>
-            </div>
-
-
-            <div className='bg-zinc-900 w-[100%] after:rounded-lg after:duration-300 after:w-1 after:inline-flex after:h-[1px] after:bg-white hover:after:w-full rounded-xl'>
-                <div className='flex justify-between items-center p-4'>
-                    <h1 className='text-left font-mono text-[1.3rem]'>Project Name</h1>
-                    <a className='font-mono text-[.8rem] tracking-wider' href='#' target='_blank'>View Project</a>
-                </div>
-                
-                <p className='p-4 font-mono text-justify text-[.9rem] text-gray-300'>Cupidatat in tempor nulla irure eiusmod aliqua incididunt velit. Deserunt Lorem reprehenderit ullamco ea id sit ex enim nulla mollit tempor dolore.</p>
-                <div className='flex justify-between items-center px-4'>
-                    <div className='flex items-center'>
-                        <div className='w-2 h-2 rounded-full bg-blue-500'></div>
-                        <p className='ml-3 font-mono text-[.9rem] tracking-widest'>ReactJS</p>
-                    </div>
-                    <a className='font-mono text-[.8rem] tracking-wider' href='#' target='_blank'>GitHub</a>
-                </div>
-            </div>
-
+            
 
         </div>
       </div>
