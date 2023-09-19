1429fdd10e7afcc25c2414ef20fcb8d5473e7bd9
Victory Odumeh <vodumeh@gmail.com>
2023-09-19 14:49:02 +0100

    ready for launch 🚀

--- src/Components/Left/Left.jsx
+++ src/Components/Left/Left.jsx
@@ -9,7 +9,7 @@ const Left = () => {
             <span className='textOne text-[1rem] md:text-[1.4rem] lg:text-[2.3rem] text-white p-3 font-mono tracking-widest'>
                 Victory Odumeh .K.
             </span>
-            <p className='duration-300 textTwo text-sans text-zinc-600 text-[.8rem] pl-[10%] md:text-[1.2rem] lg:text-[1.2rem] tracking-widest'>Solution-driven 
+            <p className='duration-300 textTwo text-sans text-zinc-600 text-[.8rem] pl-[10%] md:text-[1.2rem] lg:text-[1.2rem] tracking-widest text-center'>Solution-driven 
                  Full-Stack Developer with experience in developing stunning web applications using technologies such as <span className='text-yellow-500 underline'>JavaScript</span>, <span className='text-pink-500 underline'>Sass</span>, <span className='text-purple-500 underline'>Bootstrap</span>, <span className='text-blue-500 underline'>ReactJS <span className='text-white'>,</span> TailwindCSS</span> and <span className='text-purple-700 underline'>PHP/<span className='text-yellow-500 underline'>MySQL</span></span>.
             </p>
         </div>
