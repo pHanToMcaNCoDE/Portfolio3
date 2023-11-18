58d45dc58bb4a2539d9c22a458368084eec7e5b9
Victory Odumeh <vodumeh@gmail.com>
2023-11-18 13:43:59 +0100

    ready for launch 🚀

--- src/Components/Left/Left.jsx
+++ src/Components/Left/Left.jsx
@@ -15,27 +15,27 @@ const Left = () => {
         </div>
             <div className='textThree flex list-none'>
                 <li className='mx-auto my-[8%] duration-300 hover:scale-110 text-white cursor-pointer'>
-                    <a href='https://www.linkedin.com/in/victory-odumeh-421761223/'>
+                    <a target='_blank' href='https://www.linkedin.com/in/victory-odumeh-421761223/'>
                         <FaLinkedinIn size={40} className='border border-white hover:border hover:bg-purple-600 hover:text-white hover:border-purple-600 rounded-full p-2 animate-bounce'/>
                     </a>
                 </li>
                 <li className='mx-auto my-[8%] duration-300 hover:scale-110 text-white hover:text-purple-600 cursor-pointer'>
-                    <a href='https://www.instagram.com/_kachi.dev/' target='_blank'>
+                    <a target='_blank' href='https://www.instagram.com/_kachi.dev/'>
                         <FaInstagram size={40} className='border border-white hover:border hover:bg-purple-600 hover:text-white hover:border-purple-600 rounded-full p-2 animate-bounce'/>
                     </a>
                 </li>
                 <li className='mx-auto my-[8%] duration-300 hover:scale-110 text-white hover:text-purple-600 cursor-pointer'>
-                    <a href='https://twitter.com/phantomcancode' target='_blank'>
+                    <a target='_blank' href='https://twitter.com/phantomcancode'>
                         <FaTwitter size={40} className='border border-white hover:border hover:bg-purple-600 hover:text-white hover:border-purple-600 rounded-full p-2 animate-bounce'/>
                     </a>
                 </li>
                 <li className='mx-auto my-[8%] duration-300 hover:scale-110 text-white hover:text-purple-600 cursor-pointer'>
-                    <a href='https://web.facebook.com/victory.odumeh' target='_blank'>
+                    <a target='_blank' href='https://web.facebook.com/victory.odumeh'>
                         <FaFacebookF size={40} className='border border-white hover:border hover:bg-purple-600 hover:text-white hover:border-purple-600 rounded-full p-2 animate-bounce'/>
                     </a>
                 </li>
                 <li className='mx-auto my-[8%] duration-300 hover:scale-110 text-white hover:text-purple-600 cursor-pointer'>
-                    <a href='https://github.com/pHanToMcaNCoDE' target='_blank'>
+                    <a target='_blank' href='https://github.com/pHanToMcaNCoDE'>
                         <FaGithub size={40} className='border border-white hover:border hover:bg-purple-600 hover:text-white hover:border-purple-600 rounded-full p-2 animate-bounce'/>
                     </a>
                 </li>
